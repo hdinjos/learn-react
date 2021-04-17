4ea336b4cb4388ea84b2dca54a9fa51fda9b0a48
hdinjos <hdinjos@gmail.com>
2021-04-17 15:57:07 +0700

    setup basic react typescript mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,103 @@
-import React from 'react';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [name, setName] = useState([{ name: "hendi", age: 34 }]);
+  const [val, setVal] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState([
+    {
+      id: "",
+      name: "",
+      username: "",
+      email: "",
+      address: {
+        street: "",
+        suite: "",
+        city: "",
+        zipcode: "",
+        geo: {
+          lat: "",
+          lng: "",
+        },
+      },
+      phone: "",
+      website: "",
+      company: {
+        name: "",
+        catchPhrase: "",
+        bs: "",
+      },
+    },
+  ]);
+  useEffect(() => {
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        setUser(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const evaluate = () => {
+    setName((state) => [...state, { name: val, age: 20 }]);
+  };
+
+  const style = {
+    button: {
+      paddingTop: "10px",
+    },
+    listName: {
+      marginLeft: "20px",
+      backgroundColor: "blue",
+      color: "white",
+      width: "300px",
+      fontWeight: 900,
+      paddingLeft: "50px",
+      // paddingRight: "20px",
+    },
+  };
+
   return (
     <div>
-     
+      <input
+        value={val}
+        type="text"
+        onChange={(e) => setVal(e.target.value)}
+        placeholder="inputan"
+      />
+      <p>hasilnya {val}</p>
+      <div style={style.button}>
+        <button onClick={evaluate}>Tambah Nama</button>
+      </div>
+      <h2>Daftar nama</h2>
+      {name.map((item, i) => {
+        return (
+          // <div key={i}>{item.age ? <p>Muncul</p> : <p>Tidak Muncul</p>}</div>
+          <div style={style.listName} key={i}>
+            {item.name}
+          </div>
+        );
+      })}
+      <h4>Hasil user dari jsonplaceholder</h4>
+
+      {(() => {
+        if (loading) {
+          return <h4>Loading...</h4>;
+        } else {
+          return (
+            <div>
+              {user.map((item) => {
+                return (
+                  <ul key={item.id}>
+                    <li>{item.name}</li>
+                  </ul>
+                );
+              })}
+            </div>
+          );
+        }
+      })()}
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+//for primereact
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
